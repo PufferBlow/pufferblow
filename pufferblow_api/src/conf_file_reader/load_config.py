2944d5c312463f8332fe35e326168f63904904ae
0RaMsY0 <0ramsy0@gmail.com>
2023-06-02 12:30:53 +0100

    Added a config reader

---
+++ pufferblow_api/src/conf_file_reader/load_config.py
@@ -0,0 +1,16 @@
+import os
+import yaml
+
+from pufferblow_api import constants
+
+def load_config() -> dict:
+    """ Returns the config file content in form of a dict """
+    if os.path.exists(constants.PUFFERBLOW_CONFIG_PATH) != True:
+        os.mkdir(constants.PUFFERBLOW_CONFIG_PATH.replace("/config.yaml", ""))
+        with open(constants.PUFFERBLOW_CONFIG_PATH, "w") as pufferblow_config_file:
+            pufferblow_config_file.write(constants.PUFFERBLOW_CONFIG)
+    
+    with open(constants.PUFFERBLOW_CONFIG_PATH, "r") as config_file_content:
+        config_file_content = yaml.safe_load(config_file_content)
+
+    return config_file_content
