b056c747ece985647e103cc9b0286fbe1d2bed01
0RaMsY0 <0ramsy0@gmail.com>
2023-06-06 15:46:19 +0100

    Updated pufferblow-api's config model to suport supabase

--- pufferblow_api/src/models/pufferblow_api_config_model.py
+++ pufferblow_api/src/models/pufferblow_api_config_model.py
@@ -1,5 +1,6 @@
 from pufferblow_api.src.conf_file_reader.load_config import load_config
 
+
 class PufferBlowAPIConfig:
     """ PufferBlow-api's config data class """
     
@@ -10,7 +11,10 @@ class PufferBlowAPIConfig:
     LOGS_PATH           :  str     = CONFIG["api"][2]["logs_path"]
     WORKERS             :  int     = CONFIG["api"][3]["workers"]
     CONNECTION_TIMEOUT  :  int     = CONFIG["api"][4]["connection_timeout"]
-    CASSANDRA_HOST      :  str     = CONFIG["cassandra"][0]["host"]
-    CASSANDRA_PORT      :  int     = CONFIG["cassandra"][1]["port"]
-    USERNAME            :  str     = CONFIG["cassandra"][2]["username"]
-    PASSWORD            :  str     = CONFIG["cassandra"][3]["password"]
+    SUPABASE_URL        :  str     = CONFIG["supabase"][0]["supabase_url"]
+    SUPABASE_KEY        :  str     = CONFIG["supabase"][1]["supabase_key"]
+    DATABASE_NAME       :  str     = CONFIG["supabase"][2]["postregsql"][0]["database_name"]
+    USERNAME            :  str     = CONFIG["supabase"][2]["postregsql"][1]["username"]
+    DATABASE_PASSWORD   :  str     = CONFIG["supabase"][2]["postregsql"][2]["password"]
+    DATABASE_HOST       :  str     = CONFIG["supabase"][2]["postregsql"][3]["host"]
+    DATABASE_PORT       :  int     = CONFIG["supabase"][2]["postregsql"][4]["port"]   
