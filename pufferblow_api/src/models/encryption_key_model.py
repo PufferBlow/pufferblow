a539f05748899c956231db958c0e912aebf170b3
0RaMsY0 <0ramsy0@gmail.com>
2023-06-06 15:46:35 +0100

    Added an ```Encryption Key``` model

---
+++ pufferblow_api/src/models/encryption_key_model.py
@@ -0,0 +1,36 @@
+import json
+
+class EncryptionKey (object):
+    """ Encryption key data class """
+    key_value           :           str
+    salt                :           str
+    associated_to       :           str
+    user_id             =           None
+    message_id          =           None
+
+    def to_json(self) -> json.dumps:
+        """ Returns the data in json format """
+        ENCRYPTION_KEY_DATA = {
+            "key_value"             :       self.key,
+            "salt"                  :       self.salt,
+            "associated_to"         :       self.associated_to,
+            "user_id"               :       self.user_id,
+            "message_id"            :       self.message_id
+        }
+
+        return json.dumps(
+            ENCRYPTION_KEY_DATA,
+            indent=4,
+            default=str,
+            sort_keys=True
+        )
+    
+    def to_tuple(self) -> tuple:
+        """ Returns the data in tuple format """
+        return (
+            self.key_value,
+            self.salt,
+            self.associated_to,
+            self.user_id,
+            self.message_id
+        )
