8dde5012cc844e84e95060c9001a850bd83e4e8b
0RaMsY0 <0ramsy0@gmail.com>
2023-06-06 15:48:06 +0100

    Added a signup users route

--- pufferblow_api/pufferblow_api.py
+++ pufferblow_api/pufferblow_api.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import base64
 
 from fastapi import (
     FastAPI,
@@ -20,7 +21,11 @@ from pufferblow_api.src.utils.logger import (
     WORKERS,
     JSON_LOGS,
 )
+from pufferblow_api.src.hasher.hasher import Hasher
+from pufferblow_api.src.models.user_model import User
+from pufferblow_api.src.utils.user_id_generator import user_id_generator
 from pufferblow_api.src.database.database_session import DatabaseSession
+from pufferblow_api.src.database.database_handler import DatabaseHandler
 from pufferblow_api.src.models.pufferblow_api_config_model import PufferBlowAPIConfig
 
 # Init API
@@ -29,13 +34,21 @@ API = FastAPI()
 # PufferBlow-api's config data class
 PUFFERBLOW_API_CONFIG = PufferBlowAPIConfig()
 
+# Init the hasher (Responsible for encrypting and decrypting data)
+HASHER = Hasher()
+
 # Init Database Connection
 DATABASE_SESSION = DatabaseSession(
-    username    =   PUFFERBLOW_API_CONFIG.USERNAME,
-    password    =   PUFFERBLOW_API_CONFIG.PASSWORD,
-    host        =   PUFFERBLOW_API_CONFIG.CASSANDRA_HOST,
-    port        =   PUFFERBLOW_API_CONFIG.CASSANDRA_PORT
-).session()
+    supabase_url            =   PUFFERBLOW_API_CONFIG.SUPABASE_URL,
+    supabase_key            =   PUFFERBLOW_API_CONFIG.SUPABASE_KEY,
+    pufferblow_api_config   =   PUFFERBLOW_API_CONFIG
+)
+
+# Init Database handler
+DATABASE_HANDLER = DatabaseHandler(
+    database_connenction=DATABASE_SESSION.database_connection_session(),
+    hasher=HASHER
+)
 
 @API.get("/")
 def redirect_route():
@@ -44,11 +57,51 @@ def redirect_route():
 @API.get("/api/v1", status_code=200)
 def home_route():
     return {
-        "status code": 200,
+        "status_code": 200,
         "message": "Welcome to PufferBlow's API",
         "github": constants.ORG_GITHUB
     }
 
+# Users routes 
+@API.get("/api/v1/users", status_code=200)
+def users_route():
+    """ Main users route """
+    return {
+        "status_code": 200,
+        "description": "This is the main users route"
+    }
+
+@API.post("/api/v1/users/signup", status_code=201)
+async def signup_new_user(
+    username: str,
+    email: str,
+    password: str
+):
+    """ Signup a new user """
+    new_user = User()
+
+    new_user.user_id                                =       user_id_generator(DATABASE_HANDLER._users_id())
+    new_user.username                               =       username
+    new_user.email                                  =       email
+    new_user.status                                 =       "ONLINE"
+    new_user.contacts                               =       []
+    new_user.conversations                          =       []
+
+    encrypted_password, encryption_key_data     =       HASHER.encrypt(password)
+    new_user.password_hash                      =       base64.b64encode(encrypted_password).decode("ascii")
+    encryption_key_data.associated_to           =       "password"
+    encryption_key_data.user_id                 =       new_user.user_id
+
+    DATABASE_HANDLER._save_keys(encryption_key_data)
+
+    new_user.auth_token, new_user.auth_token_expire_time = DATABASE_HANDLER.sign_up(new_user)
+
+    return {
+        "status_code": 201,
+        "message": "Account created successfully",
+        "auth_token": new_user.auth_token,
+        "auth_token_expire_time": new_user.auth_token_expire_time
+    }
 
 def run() -> None:
     """ Starts the API """
