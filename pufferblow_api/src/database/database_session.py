0012607501bb99c6e13edf296e7f2768e39b695b
0RaMsY0 <0ramsy0@gmail.com>
2023-06-06 15:47:31 +0100

    Updated the database session class to support postgresql

--- pufferblow_api/src/database/database_session.py
+++ pufferblow_api/src/database/database_session.py
@@ -1,31 +1,37 @@
 import os
 import sys
+import psycopg2
+import datetime
 
-from cassandra.cluster import (
-    Cluster,
-    PlainTextAuthProvider
+from supabase import (
+    create_client,
+    Client
 )
 
+from pufferblow_api.src.models.pufferblow_api_config_model import PufferBlowAPIConfig
+
 class DatabaseSession (object):
-    def __init__(self, username, password, host, port) -> None:
-        self.username = username
-        self.password = password
-        self.host = host
-        self.port = port
+    def __init__(self, supabase_url: str, supabase_key: str, pufferblow_api_config: PufferBlowAPIConfig) -> None:
+        self.supabase_url = supabase_url
+        self.supabase_key = supabase_key
+        self.pufferblow_api_config = pufferblow_api_config
 
-    def session(self):
+    def database_connection_session(self) -> psycopg2.connect:
         """ Returns the database session """
-        cluster = Cluster(
-            [self.host],
-            port=self.port,
-            auth_provider=self._auth_provider()
-        )
-        session = cluster.connect()
+        KEEPALIVE_KWAGS = {
+            "keepalives": 7,
+            "keepalives_idle": 30,
+            "keepalives_interval": 7,
+            "keepalives_count": 7,
+        }
 
-        return session
-   
-    def _auth_provider(self) -> PlainTextAuthProvider:
-        return PlainTextAuthProvider(
-            self.username, \
-            self.password
+        database_connection = psycopg2.connect(
+            database=self.pufferblow_api_config.DATABASE_NAME,
+            host=self.pufferblow_api_config.DATABASE_HOST,
+            user=self.pufferblow_api_config.USERNAME,
+            password=self.pufferblow_api_config.DATABASE_PASSWORD,
+            port=self.pufferblow_api_config.DATABASE_PORT,
+            **KEEPALIVE_KWAGS
         )
+
+        return database_connection
