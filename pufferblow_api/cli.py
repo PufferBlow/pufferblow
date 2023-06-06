11e739b27df2a27ff983530b0fcdbb5d35ebbfc1
0RaMsY0 <0ramsy0@gmail.com>
2023-06-06 15:42:47 +0100

    Update ```cli.py```

--- pufferblow_api/cli.py
+++ pufferblow_api/cli.py
@@ -7,7 +7,10 @@ from rich.console import Console
 
 from pufferblow_api import constants
 from pufferblow_api.pufferblow_api import run
+from pufferblow_api.src.models.tests_model import Test
 from pufferblow_api.src.conf_file_reader.load_config import load_config
+
+# from pufferblow_api.tests.test_user_signup import test_user_signup
 
 # Init cli
 CLI = typer.Typer()
@@ -20,18 +23,27 @@ def version():
     """ PufferBlow's API version """
     print(f"[bold cyan] Version [italic white]{constants.VERSION}")
 
-@CLI.command()
-def tests():
-    """ Runs all the tests """
-    pass
+# @CLI.command()
+# def tests():
+#     """ Runs all the tests """
+#     tests = [
+#         test_user_signup
+#     ]
+
+#     with CONSOLE.status("[bold green] Running tests") as status:
+#         for test in tests:
+#             _test = Test(test.__name__, tests.index(test))
+#             _test.run(test)
+
+#             CONSOLE.log(f"[bold yellow] `{test.__name__}` {_test.message}")
 
 @CLI.command()
 def serve():
     """ Serves the API on the passed host and port """
     CONFIG = load_config()
 
-    if CONFIG["cassandra"][2]["username"] == "<your username>" and CONFIG["cassandra"][3]["password"] == "<your password>":
-        print(f"[bold red] [  ?  ] [bold white] Config error: please edit the [italic yellow]`username`[/][bold white] and [italic yellow]`password`[/][bold white] feilds in [bold cyan]{constants.PUFFERBLOW_CONFIG_PATH}[white]")
+    if CONFIG["supabase"][0]["supabase_url"] == "<your supabase url>" and CONFIG["supabase"][1]["supabase_key"] == "<your supabase key>":
+        print(f"[bold red] [  ?  ] [bold white] Config error: please edit the [italic yellow]`supabase_url`[/][bold white] and [italic yellow]`supabase_key`[/][bold white] feilds in [bold cyan]{constants.PUFFERBLOW_CONFIG_PATH}[white]")
         sys.exit(1)
     
     run()
