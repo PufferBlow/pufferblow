e96601d4ff88ce33746433a57086e9a0e0ac5e6a
0RaMsY0 <0ramsy0@gmail.com>
2023-06-14 02:53:47 +0100

    Update ```database_handler.py```

--- pufferblow_api/src/database/database_handler.py
+++ pufferblow_api/src/database/database_handler.py
@@ -7,12 +7,13 @@ import datetime
 import psycopg2
 import psycopg2.pool
 
+from rich import print
+
 from pufferblow_api import constants
 from pufferblow_api.src.hasher.hasher import Hasher
+from pufferblow_api.src.models.salt_model import Salt
 from pufferblow_api.src.models.user_model import User
 from pufferblow_api.src.models.encryption_key_model import EncryptionKey
-from pufferblow_api.src.utils.user_id_generator import user_id_generator
-from pufferblow_api.src.utils.auth_token_generator import auth_token_generator
 
 class DatabaseHandler (object):
     """ Database handler for PufferBlow's API """
@@ -20,54 +21,8 @@ class DatabaseHandler (object):
         self.database_connection_pool       =         database_connection_pool
         self.hasher                         =         hasher
     
-    def sign_up(self, user_data: User, is_test_mode: bool | None = False) -> None:
+    def sign_up(self, user_data: User) -> None:
         """ Signs up a new user and returns a auth token """
-        auth_token  = auth_token_generator(self._auth_tokens())
-        expire_time = datetime.date.today()
-
-        if expire_time.month != 12:
-            expire_time = expire_time.replace(
-                month=expire_time.month+1
-            )
-        else:
-            expire_time = expire_time.replace(
-                month=1,
-                year=expire_time.year+1
-            )
-        
-        user_data.raw_auth_token         = auth_token
-        user_data.auth_token_expire_time = expire_time
-
-        user_data.created_at = datetime.date.today()
-        user_data.last_seen  = datetime.datetime.now()
-
-        encrypted_username, username_encryption_key          =       self.hasher.encrypt(user_data.username)
-        encrypted_auth_token, auth_token_encryption_key      =       self.hasher.encrypt(user_data.raw_auth_token)
-        encrypted_email, email_encryption_key                =       self.hasher.encrypt(user_data.email)
-
-        username_encryption_key.associated_to, username_encryption_key.user_id =  \
-            constants.ASSOCIATIONS["user_id"][0], \
-                user_data.user_id
-        auth_token_encryption_key.associated_to, auth_token_encryption_key.user_id = \
-            constants.ASSOCIATIONS["user_id"][1], \
-                user_data.user_id
-        email_encryption_key.associated_to, email_encryption_key.user_id =  \
-            constants.ASSOCIATIONS["user_id"][2], \
-                user_data.user_id
-        
-        encryption_keys_data = [
-            username_encryption_key,
-            auth_token_encryption_key,
-            email_encryption_key
-        ]
-
-        for encryption_key_data in encryption_keys_data:
-            self._save_keys(encryption_key_data)
-        
-        user_data.username                =      base64.b64encode(encrypted_username).decode("ascii")
-        user_data.encrypted_auth_token    =      base64.b64encode(encrypted_auth_token).decode("ascii")
-        user_data.email                   =      base64.b64encode(encrypted_email).decode("ascii")
-
         database_connection = self.database_connection_pool.getconn()
         try:
             with database_connection.cursor() as cursor:
@@ -78,23 +33,87 @@ class DatabaseHandler (object):
                     user_data.to_tuple()
                 )
                 database_connection.commit()
+                
+                self.save_auth_token(
+                    user_id=user_data.user_id,
+                    auth_token=user_data.encrypted_auth_token,
+                    auth_token_expire_time=user_data.auth_token_expire_time
+                )
         finally:
             self.database_connection_pool.putconn(
                 database_connection,
                 close=False
             )
 
-        self._save_token(
-            user_data.encrypted_auth_token,
-            user_data.auth_token_expire_time,
-            user_data.user_id
-        )
+    def fetch_user_data(self, user_id: str, auth_token: str) -> tuple:
+        """ 
+            Returns a User model containing
+            information about the given user's id
+        """
+        database_connection = self.database_connection_pool.getconn()
+        user_data = None
 
-        return (user_data.raw_auth_token, user_data.auth_token_expire_time) \
-                if is_test_mode != True \
-                    else (user_data.raw_auth_token, user_data.auth_token_expire_time, user_data.user_id)
+        try:
+            with database_connection.cursor() as cursor:
+                sql = "SELECT user_id, username, email, password_hash, status, last_seen, conversations, contacts, auth_token, auth_token_expire_time, created_at FROM users WHERE user_id = %s AND auth_token = %s"
 
-    def _save_token(
+                cursor.execute(
+                    sql,
+                    (user_id, auth_token)
+                )
+                user_data = cursor.fetchone()
+        finally:
+            self.database_connection_pool.putconn(
+                database_connection,
+                close=False
+            )
+        
+        return user_data
+
+    def delete_auth_token(self, user_id: str, auth_token: str) -> None:
+        """
+        Deletes the given encrypted auth_token that belongs to the user_id
+        
+        Paramters:
+            user_id (str): The user's id
+            auth_token (str): The raw version of the auth_token to delete
+        """ 
+        database_connection = self.database_connection_pool.getconn()
+
+        try:
+            with database_connection.cursor() as cursor:
+                sql = "DELETE * FROM auth_tokens WHERE auth_token = '%s' AND user_id = '%s'"
+                cursor.execute(
+                    sql,
+                    (auth_token, user_id)
+                )
+
+                database_connection.commit()
+        finally:
+            self.database_connection_pool.putconn(
+                database_connection,
+                close=False
+            )
+    
+    def save_salt(self, salt: Salt) -> None:
+        """ Stores the salt data in the salt table """
+        database_connection = self.database_connection_pool.getconn()
+        try:
+            with database_connection.cursor() as cursor:
+                sql = "INSERT INTO salts (salt_value, hashed_data, user_id, associated_to, created_at) VALUES (%s, %s, %s, %s, %s)"
+                
+                cursor.execute(
+                    sql,
+                    salt.to_tuple()
+                )
+                database_connection.commit()
+        finally:
+            self.database_connection_pool.putconn(
+                database_connection,
+                close=False
+            )
+
+    def save_auth_token(
         self,
         auth_token: bytes,
         auth_token_expire_time: datetime.date.today,
@@ -122,42 +141,13 @@ class DatabaseHandler (object):
                 close=False
             )
 
-    def _auth_tokens(self) -> list:
-        """ Returns all the auth tokens that are still haven't expired """
-        auth_tokens = []
-        database_connection = self.database_connection_pool.getconn()
-
-        try:
-            with database_connection.cursor() as cursor:
-                sql = "SELECT user_id, auth_token FROM auth_tokens"
-                cursor.execute(sql)
-
-                users_data = cursor.fetchall()
-
-                if len(users_data) == 0:
-                    return auth_tokens
-
-                for user_data in users_data:
-                    user_id                 =   user_data[0]
-                    encrypted_auth_token    =   base64.b64decode(user_data[1])
-                    get_decryption_key_sql  =   "SELECT key_value FROM keys WHERE user_id = '%s' AND associated_to = 'auth_token'" % user_id
-
-                    cursor.execute(get_decryption_key_sql)
-                    decryption_key = cursor.fetchall()[0][0]
-
-                    auth_token = self.hasher.decrypt(encrypted_auth_token, decryption_key)
-
-                    auth_tokens.append(auth_token)
-        finally:
-            self.database_connection_pool.putconn(
-                database_connection,
-                close=False
-            )
-
-        return auth_tokens
-
-    def _users_id(self) -> list:
-        """ Returns a list of all the used users id """
+    def get_users_id(self) -> list:
+        """
+        Returns a list of all the used users id
+        
+        Returns:
+            list: A list containing all the used `user_id`
+        """
         database_connection = self.database_connection_pool.getconn()
 
         try:
@@ -179,7 +169,7 @@ class DatabaseHandler (object):
 
         return user_ids
 
-    def _save_keys(self, keys: EncryptionKey):
+    def save_encryption_key(self, key: EncryptionKey):
         """ Saves the keys that are used in encryption along side with the salt """
         database_connection = self.database_connection_pool.getconn()
         
@@ -189,9 +179,127 @@ class DatabaseHandler (object):
                 
                 cursor.execute(
                     sql,
-                    keys.to_tuple()
+                    key.to_tuple()
                 )
                 database_connection.commit()
+        finally:
+            self.database_connection_pool.putconn(
+                database_connection,
+                close=False
+            )
+    
+    def get_decryption_key(self, associated_to: str, user_id: str| None=None, message_id: str | None=None) -> bytes:
+        """ Returns the decryption key from the database """
+        database_connection = self.database_connection_pool.getconn()
+        key = None
+
+        try:
+            with database_connection.cursor() as cursor:
+                if user_id is not None:
+                    sql = "SELECT key_value FROM keys WHERE user_id = '%s' AND associated_to = '%s'" % (user_id, associated_to)
+                else:
+                    sql = "SELECT key_value FROM keys WHERE message_id = '%s' AND associated_to = '%s'" % (message_id, associated_to)
+
+                cursor.execute(sql)
+
+                key = cursor.fetchall()[0][0]
+        finally:
+            self.database_connection_pool.putconn(
+                database_connection,
+                close=False
+            )
+
+        return key
+
+    def get_salt(self, user_id: str, associated_to: str) -> bytes:
+        """
+        Returns the salt used to hash a password, or auth_token
+         
+        Parameters:
+            user_id (str): The user's id
+            associated_to (str): What is the salt associated to
+                    ["auth_token", "password"]
+
+        Returns:
+            str: salt
+        """
+        database_connection = self.database_connection_pool.getconn()
+        salt = None
+
+        try:
+            with database_connection.cursor() as cursor:
+                sql = "SELECT salt_value FROM salts WHERE user_id = %s AND associated_to = %s" 
+
+                cursor.execute(
+                    sql,
+                    (user_id, associated_to)
+                )
+                salt = cursor.fetchone()[0]
+        finally:
+            self.database_connection_pool.putconn(
+                database_connection,
+                close=False
+            )
+        
+        return base64.b64decode(salt)
+
+    def get_auth_token_expire_time(self, user_id: str, auth_token: str) -> str:
+        """
+        Returns the expire time for the given auth token
+        
+        Parameters:
+            auth_token (str): Encrypted version of the auth token
+        
+        Returns:
+            str: Expire time
+        """
+        database_connection = self.database_connection_pool.getconn()
+        expire_time = None
+
+        try:
+            with database_connection.cursor() as cursor:
+                sql = "SELECT auth_token_expire_time FROM auth_tokens WHERE user_id = '%s' AND auth_token = '%s'" % (auth_token, user_id)
+                cursor.execute(sql)
+
+                expire_time = cursor.fetchone()[0]
+        
+        finally:
+            self.database_connection_pool.putconn(
+                database_connection,
+                close=False
+            )
+        
+        return expire_time
+    
+    def check_auth_token(self, hashed_auth_token: bytes, user_id: str) -> bool:
+        """
+        Checks the validity of the `auth_token`
+        
+        Parameters:
+            user_id (str): The user's `user_id`
+            hashed_auth_token (bytes): The hash user's `auth_token`
+        
+        Returns:
+            bool: True if the `auth_token` exists, otherwise False
+        """
+        database_connection = self.database_connection_pool.getconn()
+
+        try:
+            with database_connection.cursor() as cursor:
+                sql = "SELECT * FROM users WHERE user_id = %s AND auth_token = %s"
+
+                cursor.execute(
+                    sql,
+                    (user_id, hashed_auth_token)
+                )
+
+                auth_token = cursor.fetchall()
+                print(f"check auth-token | {auth_token = } | {hashed_auth_token = }")
+
+                if len(auth_token) != 1:
+                    return False
+                
+                return True
         finally:
             self.database_connection_pool.putconn(
                 database_connection,
