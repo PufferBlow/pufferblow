53b202d03575f3c5ec705e55d6673226fa6daae3
0RaMsY0 <0ramsy0@gmail.com>
2023-06-14 02:54:26 +0100

    Update ```pufferblow_api.py```

--- pufferblow_api/pufferblow_api.py
+++ pufferblow_api/pufferblow_api.py
@@ -4,7 +4,8 @@ import base64
 
 from fastapi import (
     FastAPI,
-    responses
+    responses,
+    exceptions
 )
 
 from gunicorn.app.base import BaseApplication
@@ -21,9 +22,10 @@ from pufferblow_api.src.utils.logger import (
     WORKERS,
     JSON_LOGS,
 )
+
 from pufferblow_api.src.hasher.hasher import Hasher
-from pufferblow_api.src.models.user_model import User
-from pufferblow_api.src.utils.user_id_generator import user_id_generator
+from pufferblow_api.src.user.user_manager import UserManager
+from pufferblow_api.src.auth.auth_token_manager import AuthTokenManager
 from pufferblow_api.src.database.database_session import DatabaseSession
 from pufferblow_api.src.database.database_handler import DatabaseHandler
 from pufferblow_api.src.models.pufferblow_api_config_model import PufferBlowAPIConfig
@@ -46,8 +48,21 @@ DATABASE_SESSION = DatabaseSession(
 
 # Init Database handler
 DATABASE_HANDLER = DatabaseHandler(
-    database_connection_pool=DATABASE_SESSION.database_connection_pool(),
-    hasher=HASHER
+    database_connection_pool    =       DATABASE_SESSION.database_connection_pool(),
+    hasher                      =       HASHER
+)
+
+# Init Auth tokens manager
+AUTH_TOKEN_MANAGER = AuthTokenManager(
+    database_handler        =       DATABASE_HANDLER,
+    hasher                  =       HASHER
+)
+
+# Init user manager
+USER_MANAGER = UserManager(
+    database_handler        =       DATABASE_HANDLER,
+    auth_token_manager      =       AUTH_TOKEN_MANAGER,
+    hasher                  =       HASHER
 )
 
 @API.get("/")
@@ -78,30 +93,84 @@ async def signup_new_user(
     password: str
 ):
     """ Signup a new user """
-    new_user = User()
-
-    new_user.user_id                                =       user_id_generator(DATABASE_HANDLER._users_id())
-    new_user.username                               =       username
-    new_user.email                                  =       email
-    new_user.status                                 =       "ONLINE"
-    new_user.contacts                               =       []
-    new_user.conversations                          =       []
-
-    encrypted_password, encryption_key_data     =       HASHER.encrypt(password)
-    new_user.password_hash                      =       base64.b64encode(encrypted_password).decode("ascii")
-    encryption_key_data.associated_to           =       "password"
-    encryption_key_data.user_id                 =       new_user.user_id
-
-    new_user.raw_auth_token, new_user.auth_token_expire_time = DATABASE_HANDLER.sign_up(new_user)
-    
-    DATABASE_HANDLER._save_keys(encryption_key_data)
+    user_data = USER_MANAGER.sign_up(
+        username=username,
+        email=email,
+        password=password
+    )
 
     return {
         "status_code": 201,
         "message": "Account created successfully",
-        "auth_token": new_user.raw_auth_token,
-        "auth_token_expire_time": new_user.auth_token_expire_time
+        "user_id": user_data.user_id,
+        "auth_token": user_data.raw_auth_token,
+        "auth_token_expire_time": user_data.auth_token_expire_time
     }
+
+@API.get("/api/v1/users/profile", status_code=200)
+async def users_profile_route(
+    user_id: str,
+    auth_token: str,
+):
+    """ Users profile management route """
+    if user_id not in DATABASE_HANDLER.get_users_id():
+        raise exceptions.HTTPException(
+            status_code=404,
+            detail=f"\"{user_id}\" not found. Please make sure to pass the correct one"
+        )
+    hashed_auth_token = AUTH_TOKEN_MANAGER._encrypt_auth_token(
+        user_id=user_id,
+        auth_token=auth_token
+    )
+    if not AUTH_TOKEN_MANAGER.token_exists(
+        user_id=user_id,
+        hashed_auth_token=hashed_auth_token
+    ):
+        raise exceptions.HTTPException(
+            status_code=404,
+            detail=f"\"{auth_token}\" not found, or expired. Please make sure to pass the correct one"
+        )
+
+    user_data = USER_MANAGER.user_profile(
+        user_id=user_id,
+        hashed_auth_token=hashed_auth_token
+    )
+
+    return {
+        "status_code": 200,
+        "user_data": user_data
+    }
+
+@API.put("/api/v1/users/profile", status_code=201)
+async def edit_users_profile_route(
+    user_id: str,
+    auth_token: str,
+    username: str,
+    status: str,
+    email: str,
+    old_email: str,
+    password: str,
+    old_password: str
+):
+    """ Edits a user's profile data such as: status,
+    last_seen, username, email and password
+    
+    Parameters:
+        user_id (str): The user's id
+        auth_token (str): The user's auth_token
+        username (str): The new username for the user
+        status (str): The new status for the user ["ONLINE", "OFFLINE"]
+        email (str): The new email for the user
+        password (str): The new password for the user
+        old_password (str): The old password of the user. This is in case the ```password``` was passed 
+    
+    Returns:
+        {
+            "status_code": 201,
+            "message": (str)
+        }
+    """
+    pass
 
 def run() -> None:
     """ Starts the API """
@@ -131,6 +200,7 @@ def run() -> None:
     OPTIONS = {
         "bind": f"{PUFFERBLOW_API_CONFIG.API_HOST}:{PUFFERBLOW_API_CONFIG.API_PORT}",
         "workers": WORKERS(PUFFERBLOW_API_CONFIG.WORKERS),
+        "reload": True,
         "timeout": PUFFERBLOW_API_CONFIG.CONNECTION_TIMEOUT,
         "accesslog": "-",
         "errorlog": "-",
