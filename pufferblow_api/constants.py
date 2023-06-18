9f1debcbec80dc9d93c6cdcfa1c143ac6f148bb3
0RaMsY0 <0ramsy0@gmail.com>
2023-06-18 17:14:23 +0100

    Added new logs messages

--- pufferblow_api/constants.py
+++ pufferblow_api/constants.py
@@ -45,28 +45,40 @@ ASSOCIATIONS = {
 }
 
 # Logs messages
-NEW_USER_SIGNUP_SUCCESSFULLY = lambda user: f"New user signup: user_id=\"{user.user_id}\""
-REQUEST_FOR_USER_PROFILE     = lambda user_data, viewer_user_id: f"User profile requested: viewer=\"{viewer_user_id}\", target=\"{user_data['user_id']}\", user_data={user_data}"
+NEW_USER_SIGNUP_SUCCESSFULLY = lambda user: f"New user signed up successfully. User ID: '{user.user_id}'"
+REQUEST_FOR_USER_PROFILE = lambda user_data, viewer_user_id: f"Requested user profile. Viewer: '{viewer_user_id}', Target: '{user_data['user_id']}', User Data: {user_data}"
 
-NEW_USER_ID_GENERATED = lambda user_id: f"New user_id generated: user_id=\"{user_id}\""
+NEW_USER_ID_GENERATED = lambda user_id: f"Generated new user ID: '{user_id}'"
 
-NEW_AUTH_TOKEN_GENERATED = lambda auth_token: f"New auth_token generated: auth_token=\"{auth_token}\""
-NEW_AUTH_TOKEN_HASHED  = lambda auth_token, hashed_auth_token, salt: f"New auth_token hashed: auth_token=\"{auth_token}\", hashed_auth_token=\"{hashed_auth_token}\", salt=\"{salt.salt_value}\""
-NEW_AUTH_TOKEN_SAVED   = lambda auth_token: f"New auth_token saved to \"auth_tokens\" table: {auth_token}"
+NEW_AUTH_TOKEN_GENERATED = lambda auth_token: f"Generated new authentication token: '{auth_token}'"
+NEW_AUTH_TOKEN_HASHED = lambda auth_token, hashed_auth_token, salt: f"Hashed new authentication token. Auth token: '{auth_token}', Hashed token: '{hashed_auth_token}', Salt: '{salt.salt_value}'"
+NEW_AUTH_TOKEN_SAVED = lambda auth_token: f"Saved new authentication token in 'auth_tokens' table: {auth_token}"
 
-NEW_DERIVED_KEY_CREATED = lambda user, key: f"New derived key created: user_id=\"{user.user_id}\", key=\"{key.key_value}\", salt=\"{key.salt}\""
-NEW_DERIVED_KEY_SAVED   = lambda key: f"New derived key saved to \"keys\": {key.to_json()}"
+NEW_DERIVED_KEY_CREATED = lambda user, key: f"Created new derived key. User ID: '{user.user_id}', Key: '{key.key_value}'"
+NEW_DERIVED_KEY_SAVED = lambda key: f"Saved new derived key in 'keys': {key.to_json()}"
+DERIVED_KEY_DELETED = lambda key: f"Deleted derived key from 'keys': {key.to_json()}"
 
-NEW_HASH_SALT_CREATED = lambda salt: f"New salt created: salt=\"{salt.salt_value}\", associated_to=\"{salt.associated_to}\", hashed_data=\"{salt.hashed_data}\""
-NEW_HASH_SALT_SAVED   = lambda salt: f"New salt saved to \"salts\": {salt.to_json()}"
+NEW_HASH_SALT_CREATED = lambda salt: f"Created new hash salt. Salt: {salt.salt_value}, Associated to: '{salt.associated_to}', Hashed data: '{salt.hashed_data}'"
+NEW_HASH_SALT_SAVED = lambda salt: f"Saved new hash salt in 'salts': {salt.to_json()}"
 
-NEW_PASSWORD_HASHED = lambda password, hashed_password: f"New password hashed: password=\"{password}\", hashed_password=\"{hashed_password}\""
+NEW_PASSWORD_HASHED = lambda password, hashed_password: f"Hashed new password. Password: '{password}', Hashed password: '{hashed_password}'"
 
-USERNAME_ENCRYPTED = lambda username, encrypted_username: f"Username encrypted: username=\"{username}\", encrypted_username=\"{encrypted_username}\""
-USERNAME_DECRYPTED = lambda encrypted_username, decrypted_username: f"Username decrypted: encrypted_username=\"{encrypted_username}\", decrypted_username=\"{decrypted_username}\""
+USERNAME_ENCRYPTED = lambda username, encrypted_username: f"Encrypted username. Username: '{username}', Encrypted username: '{encrypted_username}'"
+USERNAME_DECRYPTED = lambda encrypted_username, decrypted_username: f"Decrypted username. Encrypted username: '{encrypted_username}', Decrypted username: '{decrypted_username}'"
 
-VALIDATE_AUTH_TOKEN = lambda hashed_auth_token, is_valid: f"Validate auth_token: hashed_auth_token=\"{hashed_auth_token}\", is_valid=\"{is_valid}\""
+VALIDATE_AUTH_TOKEN = lambda hashed_auth_token, is_valid: f"Validated authentication token. Hashed token: '{hashed_auth_token}', Valid: {is_valid}"
 
-REQUEST_SALT_VALUE = lambda user_id, salt_value, associated_to: f"Salt requested to hash some data: user_id=\"{user_id}\", salt=\"{salt_value}\", associated_to=\"{associated_to}\""
+REQUEST_SALT_VALUE = lambda user_id, salt_value, associated_to: f"Requested salt value for hashing data. User ID: '{user_id}', Salt: {salt_value}, Associated to: '{associated_to}'"
 
-FETCH_USERS_ID = lambda users_id: f"Fetched users id: users_id={users_id}"
+FETCH_USERS_ID = lambda users_id: f"Fetched user IDs. User IDs: {users_id}"
+
+FETCH_USERNAMES = lambda usernames: f"Fetched usernames. Usernames: {usernames}"
+
+UPDATE_USERNAME = lambda user_id, new_username, old_username: f"Updated username. User ID: '{user_id}', Old username: '{old_username}', New username: '{new_username}'"
+
+UPDATE_USER_STATUS = lambda user_id, from_status, to_status: f"Updated user status. User ID: '{user_id}', From status: '{from_status}', To status: '{to_status}'"
+USER_STATUS_UPDATE_SKIPPED = lambda user_id: f"Skipped status update for User ID: '{user_id}' due to passing the same status value."
+USER_STATUS_UPDATE_FAILED = lambda user_id, status: f"Failed to update the status for User ID: '{user_id}'. Provided status value not found. Status: '{status}', Accepted status values: ['online', 'offline']."
+
+UPDATE_USER_PASSWORD = lambda user_id, hashed_old_password, hashed_new_password: f"Updated password. User ID: '{user_id}', Hashed old password: '{hashed_old_password}', Hashed new password: '{hashed_new_password}'"
+UPDATE_USER_PASSWORD_FAILED = lambda user_id: f"Failed to update the password for User ID: '{user_id}' due to an invalid old password."
