b7cb150029dad4a4fabcf56f75871e7b9697b051
ramsy <0ramsy0@gmail.com>
2023-06-29 01:47:52 +0100

    Update ```cli.py```

--- pufferblow_api/cli.py
+++ pufferblow_api/cli.py
@@ -1,54 +1,80 @@
-import os
 import sys
 import typer
 
 from rich import print
+from loguru import logger
 from rich.console import Console
 
+from pufferblow_api.src.utils.logger import (
+    InterceptHandler,
+    logging,
+    StandaloneApplication,
+    StubbedGunicornLogger,
+    LOG_LEVEL,
+    WORKERS,
+    JSON_LOGS,
+)
 from pufferblow_api import constants
-from pufferblow_api.pufferblow_api import run
-from pufferblow_api.src.models.tests_model import Test
-from pufferblow_api.src.conf_file_reader.load_config import load_config
-
-# from pufferblow_api.tests.test_user_signup import test_user_signup
+from pufferblow_api.pufferblow_api import api
+from pufferblow_api.src.models.pufferblow_api_config_model import PufferBlowAPIConfig
 
 # Init cli
-CLI = typer.Typer()
+cli = typer.Typer()
 
 # Init console
 CONSOLE = Console()
 
-@CLI.command()
+# Get config data
+PUFFERBLOW_API_CONFIG = PufferBlowAPIConfig()
+
+@cli.command()
 def version():
     """ PufferBlow's API version """
     print(f"[bold cyan] Version [italic white]{constants.VERSION}")
 
-# @CLI.command()
-# def tests():
-#     """ Runs all the tests """
-#     tests = [
-#         test_user_signup
-#     ]
-
-#     with CONSOLE.status("[bold green] Running tests") as status:
-#         for test in tests:
-#             _test = Test(test.__name__, tests.index(test))
-#             _test.run(test)
-
-#             CONSOLE.log(f"[bold yellow] `{test.__name__}` {_test.message}")
-
-@CLI.command()
+@cli.command()
 def serve():
     """ Serves the API on the passed host and port """
-    CONFIG = load_config()
-
-    if CONFIG["supabase"][0]["supabase_url"] == "<your supabase url>" and CONFIG["supabase"][1]["supabase_key"] == "<your supabase key>":
+    if PUFFERBLOW_API_CONFIG.SUPABASE_URL == "<your supabase url>" and PUFFERBLOW_API_CONFIG.SUPABASE_KEY == "<your supabase key>":
         print(f"[bold red] [  ?  ] [bold white] Config error: please edit the [italic yellow]`supabase_url`[/][bold white] and [italic yellow]`supabase_key`[/][bold white] feilds in [bold cyan]{constants.PUFFERBLOW_CONFIG_PATH}[white]")
         sys.exit(1)
     
-    run()
+    INTERCEPT_HANDLER = InterceptHandler()
+    # logging.basicConfig(handlers=[INTERCEPT_HANDLER], level=LOG_LEVEL)
+    # logging.root.handlers = [INTERCEPT_HANDLER]
+    logging.root.setLevel(LOG_LEVEL)
 
-main = CLI()
+    SEEN = set()
+
+    for name in [
+        *logging.root.manager.loggerDict.keys(),
+        "gunicorn",
+        "gunicorn.access",
+        "gunicorn.error",
+        "uvicorn",
+        "uvicorn.access",
+        "uvicorn.error",
+    ]:
+        if name not in SEEN:
+            SEEN.add(name.split(".")[0])
+            logging.getLogger(name).handlers = [INTERCEPT_HANDLER]
+
+    logger.configure(handlers=[{"sink": sys.stdout, "serialize": JSON_LOGS}])
+    logger.add(PUFFERBLOW_API_CONFIG.LOGS_PATH, rotation="10 MB")
+    
+    OPTIONS = {
+        "bind": f"{PUFFERBLOW_API_CONFIG.API_HOST}:{PUFFERBLOW_API_CONFIG.API_PORT}",
+        "workers": WORKERS(PUFFERBLOW_API_CONFIG.WORKERS),
+        "timeout": PUFFERBLOW_API_CONFIG.CONNECTION_TIMEOUT,
+        "accesslog": "-",
+        "errorlog": "-",
+        "worker_class": "uvicorn.workers.UvicornWorker",
+        "logger_class": StubbedGunicornLogger
+    }
+
+    StandaloneApplication(api, OPTIONS).run()
+
+main = cli()
 
 if __name__ == "__main__":
     main()
