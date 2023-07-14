e984ad5de4036f53007ca085363100cce43b3276
ramsy <0ramsy0@gmail.com>
2023-07-14 20:42:20 +0100

    Update ```pufferblow_api_config_model.py```

--- pufferblow_api/src/models/pufferblow_api_config_model.py
+++ pufferblow_api/src/models/pufferblow_api_config_model.py
@@ -1,19 +1,19 @@
 from pufferblow_api.src.conf_file_reader.load_config import load_config
 
-class PufferBlowAPIConfig:
+class PufferBlowAPIconfig:
     """ PufferBlow-api's config data class """
     
-    CONFIG = load_config()
+    config = load_config()
 
-    API_HOST            :  str     = CONFIG["api"][0]["host"]
-    API_PORT            :  int     = CONFIG["api"][1]["port"]
-    LOGS_PATH           :  str     = CONFIG["api"][2]["logs_path"]
-    WORKERS             :  int     = CONFIG["api"][3]["workers"]
-    CONNECTION_TIMEOUT  :  int     = CONFIG["api"][4]["connection_timeout"]
-    SUPABASE_URL        :  str     = CONFIG["supabase"][0]["supabase_url"]
-    SUPABASE_KEY        :  str     = CONFIG["supabase"][1]["supabase_key"]
-    DATABASE_NAME       :  str     = CONFIG["supabase"][2]["postregsql"][0]["database_name"]
-    USERNAME            :  str     = CONFIG["supabase"][2]["postregsql"][1]["username"]
-    DATABASE_PASSWORD   :  str     = CONFIG["supabase"][2]["postregsql"][2]["password"]
-    DATABASE_HOST       :  str     = CONFIG["supabase"][2]["postregsql"][3]["host"]
-    DATABASE_PORT       :  int     = CONFIG["supabase"][2]["postregsql"][4]["port"]   
+    API_HOST            :  str     = config["api"][0]["host"]
+    API_PORT            :  int     = config["api"][1]["port"]
+    LOGS_PATH           :  str     = config["api"][2]["logs_path"]
+    WORKERS             :  int     = config["api"][3]["workers"]
+    CONNECTION_TIMEOUT  :  int     = config["api"][4]["connection_timeout"]
+    SUPABASE_URL        :  str     = config["supabase"][0]["supabase_url"]
+    SUPABASE_KEY        :  str     = config["supabase"][1]["supabase_key"]
+    DATABASE_NAME       :  str     = config["supabase"][2]["postregsql"][0]["database_name"]
+    USERNAME            :  str     = config["supabase"][2]["postregsql"][1]["username"]
+    DATABASE_PASSWORD   :  str     = config["supabase"][2]["postregsql"][2]["password"]
+    DATABASE_HOST       :  str     = config["supabase"][2]["postregsql"][3]["host"]
+    DATABASE_PORT       :  int     = config["supabase"][2]["postregsql"][4]["port"]   
