2efb93bd87e8ae1bea73a5b92471b01bc163d68a
ramsy0dev <0ramsy0@gmail.com>
2024-01-14 22:40:51 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -25,6 +25,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_book_theme'
+html_css_files = [
+    "css/styles.css"
+]
 html_static_path = ['_static']
 
 html_title = "pufferblow-api"
