4690da00dde3ddbde2cac67c99be680a41cd669a
ramsy0dev <ramsy0dev@gmail.com>
2025-01-18 12:42:50 -0800

    Update logger.py

--- pufferblow/src/logger/logger.py
+++ pufferblow/src/logger/logger.py
@@ -4,37 +4,36 @@ import platform
 
 from loguru import logger
 
-
 JSON_LOGS = True if os.environ.get("JSON_LOGS", "0") == "1" else False
 
 def WORKERS(workers_number) -> int:
     return int(os.environ.get("GUNICORN_WORKERS", workers_number))
 
 # NOTE: we init these classes without implementation because they are linux compatible only.
-class InterceptHandler: ...
+class InterceptHandler(logging.Handler):
+    def emit(self, record):
+        # Get corresponding Loguru level if it exists
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        # Find caller from where originated the logged message
+        frame, depth = logging.currentframe(), 2
+        while frame.f_code.co_filename == logging.__file__:
+            frame = frame.f_back
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
+
 class StubbedGunicornLogger: ...
 class StandaloneApplication: ...
 
 if platform.system() == "Windows":
     pass
-else:
+elif platform.system() == "Linux":
     from gunicorn.app.base import BaseApplication
     from gunicorn.glogging import Logger
-    class InterceptHandler(logging.Handler):
-        def emit(self, record):
-            # Get corresponding Loguru level if it exists
-            try:
-                level = logger.level(record.levelname).name
-            except ValueError:
-                level = record.levelno
-
-            # Find caller from where originated the logged message
-            frame, depth = logging.currentframe(), 2
-            while frame.f_code.co_filename == logging.__file__:
-                frame = frame.f_back
-                depth += 1
-
-            logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
 
     class StubbedGunicornLogger(Logger):
         log_level: str = None
